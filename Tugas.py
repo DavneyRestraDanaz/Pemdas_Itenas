c03436f03a3113190a7dee8e4b105a31538d8d53
DavneyRestraDanaz <davney.restra@mhs.itenas.ac.id>
2023-12-09 12:14:11 +0700

    Tugas Git dan API

---
+++ Tugas.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+data = {
+    "Nama": ["John", "Jane", "Bob", "Alice"],
+    "Usia": [25, 35, 30, 28],
+    "Gaji": [50000, 60000, 70000, 55000],
+}
+
+df = pd.DataFrame(data)
+
+# Pertanyaan 1:
+# Gunakan loop for dan fungsi lambda untuk menghitung gaji setiap karyawan setelah diberikan peningkatan sebesar 5% dari gaji saat ini.
+for x, row in df.iterrows():
+    df.at[x, "Gaji naik 5%"] = row["Gaji"] * 1.05
+
+# Pertanyaan 2:
+# Setelah perubahan dilakukan, tampilkan DataFrame yang sudah diperbarui dan berikan ringkasan perubahan yang telah terjadi.
+print("\nHasil data setelah gaji naik 5%:")
+print(df)
+print("\nRingkasan perubahan")
+ringkasan = df.describe()
+print(ringkasan)
+
+# Pertanyaan 3:
+# Gunakan loop for lagi untuk mengevaluasi karyawan yang usianya di atas 30 tahun. Jika usia karyawan di atas 30, berikan peningkatan tambahan sebesar 2% dari gaji saat ini menggunakan fungsi lambda.
+for x, row in df.iterrows():
+    df.at[x, "Gaji naik 2%"] = row["Gaji"] * 1.02 if row["Usia"] > 30 else row["Gaji"]
+
+# Pertanyaan 4:
+# Tampilkan DataFrame yang sudah diperbarui setelah peningkatan gaji tambahan dan berikan ringkasan hasilnya.
+print("\nHasil data setelah gaji naik 2% (usia diatas > 30):")
+print(df)
+print("\nRingkasan perubahan")
+ringkasan = df.describe()
+print(ringkasan)
+
+# ---------------------------- #
+# Buat Branch Baru pada repository github berikut dengan format KELAS_NRP_NAMA
+# https://github.com/diashfirdaus-cyber/Pemdas_Itenas.git
+# ---------------------------- #
+
+# Catatan:
+# Gunakan loop for dan fungsi lambda untuk mengimplementasikan operasi yang diminta.
+# Pastikan untuk menyimpan hasil perubahan pada DataFrame.
+# Tampilkan hasil dan ringkasan dengan jelas.
+# Jangan lupa untuk menyesuaikan persentase peningkatan gaji sesuai dengan cerita.
